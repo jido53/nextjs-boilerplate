5496c795ce68864066d41fec8a02d5cb90f8444d
jido <gbuhl53@gmail.com>
2024-09-27 15:30:46 -0300

    tercer commit

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Relator IA, llevando el derecho al mas aIA!!!!</li>
+          <li>Relator IA, llevando el derecho al mas aIA!!!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
